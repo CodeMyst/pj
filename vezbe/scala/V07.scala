9f5646817b105dba9b4940126d46b23e6ac03489
CodeMyst <code@myst.rs>
2022-12-21 09:16:06 +0100

    V07 - nicer code
    Signed-off-by: CodeMyst <code@myst.rs>

--- vezbe/scala/V07.scala
+++ vezbe/scala/V07.scala
@@ -1,6 +1,6 @@
 object V07 {
   def main(args: Array[String]): Unit = {
-    val schools = List(new MiddleSchool("school 1", 69), new SecondarySchool("school 2", 420))
+    val schools = List(MiddleSchool("school 1", 69), SecondarySchool("school 2", 420))
 
     printAll(schools)
 
@@ -8,7 +8,7 @@ object V07 {
 
     printAll(onlyBig(schools))
 
-    val bodies = List(new Planet("oogabooga", 6000, true), new Satellite("ouhh", 2000, "oogabooga"))
+    val bodies = List(Planet("oogabooga", 6000, hasWater = true), Satellite("ouhh", 2000, "oogabooga"))
 
     println("---")
 
@@ -25,38 +25,36 @@ object V07 {
 
   private def satellitesOrbitingAround(satellites: List[Satellite], planet: String): List[Satellite] = satellites.filter(_.orbitsAround.equals(planet))
 
-  private def filterz(bodies: List[CelestialBody]): List[CelestialBody] = bodies.filter(
-    _ match {
-      case p: Planet => p.hasWater && p.radius >= 5500
-      case s: Satellite => s.radius >= 1000
-    }
-  )
+  private def filterz(bodies: List[CelestialBody]): List[CelestialBody] = bodies.filter {
+    case Planet(_, radius, hasWater) => radius >= 5500 && hasWater
+    case Satellite(_, radius, _) => radius >= 1000
+  }
 }
 
 abstract class School(val name: String, val students: Int)
 
-class MiddleSchool(override val name: String, override val students: Int)
+case class MiddleSchool(override val name: String, override val students: Int)
   extends School(name, students) {
 
-  override def toString: String = "Middle school " + name + " has " + students + " students."
+  override def toString: String = s"Middle school $name has $students students."
 }
 
-class SecondarySchool(override val name: String, override val students: Int)
+case class SecondarySchool(override val name: String, override val students: Int)
   extends School(name, students) {
 
-  override def toString: String = "Secondary school " + name + " has " + students + " students."
+  override def toString: String = s"Secondary school $name has $students students."
 }
 
 abstract class CelestialBody(val name: String, val radius: Float)
 
-class Planet(override val name: String, override val radius: Float, val hasWater: Boolean)
+case class Planet(override val name: String, override val radius: Float, hasWater: Boolean)
   extends CelestialBody(name, radius) {
 
-  override def toString: String = "Planet " + name + ", radius: " + radius + "km, has water: " + hasWater
+  override def toString: String = s"Planet $name, radius: ${radius}km, has water: $hasWater"
 }
 
-class Satellite(override val name: String, override val radius: Float, val orbitsAround: String)
+case class Satellite(override val name: String, override val radius: Float, orbitsAround: String)
   extends CelestialBody(name, radius) {
 
-  override def toString: String = "Satellite " + name + ", radius: " + radius + "km, orbits around: " + orbitsAround
+  override def toString: String = s"Satellite $name, radius: ${radius}km, orbits around: $orbitsAround"
 }
